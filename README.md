3cfadcde7d4192c7d82f98d95e6251bfcbdc43fc
Dragon-Fish <824399619@qq.com>
2021-03-01 22:43:41 +0800

    v1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# koishi-plugin-iqdb
+# iqdb for Koishi
+
+**Koishi iqdb 搜图插件**（`koishi-plugin-iqdb` ~~征名启事~~）是一个为[koishi](https://github.com/koishijs/koishi)设计的使用<iqdb.org>进行搜图的插件。
+
+## 安装
+
+安装插件，详见 [官方指南](https://koishi.js.org/guide/context.html)。
+
+```bash
+# Via yarn
+yarn add koishi-plugin-iqdb
+# Or via npm
+npm install koishi-plugin-iqdb
+```
+
+## 需求
+
+~~有网就行。~~
+
+## 配置
+
+本插件有以下配置项。
+
+### `alias`
+
+为指令配置别名。您也可以直接使用`App.command('iqdb').alias()`配置此项。
+
+类型：`String[]`
+
+默认：`[]`
+
+### `commandAuthority`
+
+操作搜图所需的权限。
+
+类型：`Number`
+
+默认：`1`
+
+### `unsafeAuthority`
+
+允许不健全的图片结果显示（`--unsafe`）所需的权限。
+
+类型：`Number`
+
+默认：`1`
--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-const iqdb = require('iqdb-client')
+const { searchPic } = require('iqdb-client')
 const { segment } = require('koishi-utils')
 
-function apply(koishi, options) {
+function apply(koishi, pluginOpt) {
+  let defaultOpt = {
+    alias: [],
+    commandAuthority: 1,
+    unsafeAuthority: 1,
+  }
+  pluginOpt = Object.assign(defaultOpt, pluginOpt)
+
   koishi
-    .command('iqdb [image:text] iqdb.org image search')
+    .command('iqdb <image:text>', { authority: pluginOpt.commandAuthority })
+    .usage('使用iqdb.org搜图')
+    .alias(...pluginOpt.alias)
+    .option('unsafe', '-u 允许不健全的图片结果显示', {
+      authority: pluginOpt.unsafeAuthority,
+    })
     .action(async ({ session }, image) => {
-      if (!image) return 'No image given'
-      let url = extractImages(session.message)
-      if (url.length < 1) return '未检测到图片'
+      let finalImage = image
+      if (!image) {
+        session.send('请在15秒内发送图片~')
+        finalImage = await session.prompt(15 * 1000)
+      }
+
+      let url = extractImages(finalImage)
+      console.log(url)
+      if (url.length < 1) return image ? '没有检测到图片' : null
       if (url.length > 1) return '图片数量过多'
 
-      let msg = await makeSearch(url[0])
-      session.send(segment('quote', { id: session.messageId }) + msg)
+      let msg = await makeSearch(url[0], pluginOpt)
+      session.send(segment('quote', { id: session.messageId }) + ' ' + msg)
     })
 }
 
@@ -26,15 +44,22 @@ function extractImages(message) {
   return result
 }
 
-async function makeSearch(url) {
-  const res = await iqdb(url, { lib: 'www' })
-  if (res.ok && res.data && res.data.length > 1) {
+async function makeSearch(url, options) {
+  const res = await searchPic(url, { lib: 'www' })
+  console.log(res)
+  if (res.ok || (res.data && res.data.length > 1)) {
     let data = res.data[1]
     let { head, sourceUrl, img, type, source } = data
-    return `标题：${head}
+    let showImg = ''
+    if (options.unsafe || type.toLowerCase() === 'safe') {
+      showImg = segment('image', { url: 'https://iqdb.org' + img })
+    } else {
+      showImg = `预览：https://iqdb.org${img} ⚠️`
+    }
+    return `${showImg}
+准度：${head.toLowerCase()}
 来源：${sourceUrl}
-图片：${img}
-H图？${type.toLowerCase() === 'safe' ? '否' : '是'}
+色图：${type.toLowerCase() === 'safe' ? '否' : '是⚠️'}
 源站：${source.join(', ')}
 `
   } else if (res.err) {
